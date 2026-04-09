bfca6c3f0587299e03fce39ec9ccd682d83d20ba
aquilsemp-lang <aquilsemp@gmail.com>
2026-04-09 15:56:42 +0530

    Add files via upload

---
+++ RecordingDetails.jsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native';
+import React from 'react';
+
+const RecordingDetails = () => {
+  return (
+    <View>
+      <Text>RecordingDetails</Text>
+    </View>
+  );
+};
+
+export default RecordingDetails;
